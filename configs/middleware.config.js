8ae069c81e6b101f9614d9df37f259c083e1a893
adrimual <adrimual14@gmail.com>
2020-09-16 16:15:19 +0200

    solving some bugs

--- configs/middleware.config.js
+++ configs/middleware.config.js
@@ -16,7 +16,9 @@ const corsOptions = {
 module.exports = app => {
     app.use(logger('dev'))
     app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }))
     app.use(cookieParser())
     app.use(cors(corsOptions))
 }
